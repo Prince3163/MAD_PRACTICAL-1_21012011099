db860d57fe9e2302d8048a3902f30d9196b7ae4a
Student <student@gnu.ac.in>
2023-07-17 10:40:51 +0530

    First Prac Commited

--- src/main/kotlin/Practical 1.1.kt
+++ src/main/kotlin/Practical 1.1.kt
@@ -1,5 +1,21 @@
 fun main()
 {
+    val x="Hello World"
+    println("$x is constant.")
+
     var a=10
-    println("$a")
+    println("$a is Integer variable")
+
+    var b="Hello"
+    println("$b is String variable")
+
+    var c=15.30f
+    println("$c is Double variable")
+
+    var d='P'
+    println("$d is Character variable")
+
+    var e=false
+    println("$e is Boolian variable")
+
 }
