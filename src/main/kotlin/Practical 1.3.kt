705b68a63c4f6c29d460531dd33b9774201f110a
student <student@gnu.ac.in>
2023-07-18 10:39:27 +0530

    Prac 1.3

---
+++ src/main/kotlin/Practical 1.3.kt
@@ -0,0 +1,31 @@
+import javax.lang.model.element.Name
+
+fun main()
+{
+    print("Enter Enrollment No : ")
+    var Enroll= readLine()
+    print("Enter Name : ")
+    var Name= readLine()
+    print("Enter Branch : ")
+    var Branch= readLine()
+    print("Enter Class : ")
+    var Class= readLine()
+    print("Enter Batch : ")
+    var Batch= readLine()
+    print("Enter College Name : ")
+    var CollegeName= readLine()
+    print("Enter University Name : ")
+    var UniversityName= readLine()
+    print("Enter Age : ")
+    var Age= readLine()
+
+    println("\nStudent Data : \n")
+    println("Enrollment No : $Enroll")
+    println("Name : $Name")
+    println("Branch : $Branch")
+    println("Class : $Class")
+    println("Batch : $Batch")
+    println("College Name : $CollegeName")
+    println("University Name : $UniversityName")
+    println("Age : $Age")
+}
