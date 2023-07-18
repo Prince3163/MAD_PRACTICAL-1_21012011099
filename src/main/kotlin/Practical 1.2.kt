d0e2c96201535b20abbdc1641908b20d511dd945
student <student@gnu.ac.in>
2023-07-18 10:11:01 +0530

    Prac 1.2

--- src/main/kotlin/Practical 1.2.kt
+++ src/main/kotlin/Practical 1.2.kt
@@ -1,5 +1,15 @@
+//Integer to Double, String to Integer, String to Double.
 fun main()
 {
-    println("18/077777")
+    var a:Int=99
+    var b=a.toDouble()
+    println("Int to Double : $b\n")
 
+    var x:String="156"
+    var y=x.toInt()
+    println("String to Int : $y\n")
+
+    var p:String="9204"
+    var q=p.toDouble()
+    println("String to Double : $q\n")
 }
