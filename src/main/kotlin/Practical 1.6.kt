61770fc533c26f7a9e7946c8379cca4290cfbdcb
Prince <patelprince3163@gmail.com>
2023-07-23 21:01:08 +0530

    23/07

--- src/main/kotlin/Practical 1.6.kt
+++ src/main/kotlin/Practical 1.6.kt
@@ -1,6 +1,19 @@
+
+fun add(x:Int ,y:Int) : Int{
+    return x+y;
+}
+fun sub(x:Int ,y:Int) : Int{
+    return x-y;
+}
+fun mul(x:Int ,y:Int) : Int{
+    return x*y;
+}
+fun div(x:Int ,y:Int) : Int{
+    return x/y;
+}
 fun main()
 {
-    print("1. Addition\n2. Substraction\n3. Multiplication\n4. Division\nChoose operation from above : ")
+    print("1. Addition\n2. Substraction\n3. Multiplication\n4. Division\nChoose one operation from above : ")
     var op=Integer.parseInt(readLine())
     print("Enter First number : ")
     var a=Integer.parseInt(readLine())
@@ -8,9 +21,10 @@ fun main()
     var b=Integer.parseInt(readLine())
     when (op)
     {
-        1->print("Add")
-        2->print("Sub")
-        3->print("Mul")
-        4->print("Div")
+        1->print("$a + $b = "+add(a,b))
+        2->print("$a - $b = "+sub(a,b))
+        3->print("$a * $b = "+mul(a,b))
+        4->print("$a / $b = "+div(a,b))
+        else->print("Invalid Operation!")
     }
 }
